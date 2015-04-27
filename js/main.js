fd2e6379ca51c203755f4ea45b06b579d1bc1385
Rahul Nadella <rahulnadella@yahoo.com>
2015-04-27 17:39:02 -0400

    Added document.ready() to main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@
 The svgColor function generates a random color for each SVG path 
 (outline of a shape) will define a new color for a revolution of 7 seconds.
 */
-var svgColor = function()
+$(function()
 {
-	/* Change the fill color of the shape (random) */
-	$('#r1').css('color', randomColor());
-	$('#r2').css('color', randomColor());
-	$('#r3').css('color', randomColor());
-	$('#r4').css('color', randomColor());
-	$('#r5').css('color', randomColor());
-	$('#r6').css('color', randomColor());
-	/* Timer for 7 seconds */
-	window.setTimeout(svgColor, 8000);
-}
-/* The svgColor function will randomily change the shape color fill */
-svgColor();  
+	/* Declare the svgColor function to set a color on the path */
+	var svgColor = function()
+	{
+		/* Change the fill color of the shape (random) */
+		$('#r1').css('color', randomColor());
+		$('#r2').css('color', randomColor());
+		$('#r3').css('color', randomColor());
+		$('#r4').css('color', randomColor());
+		$('#r5').css('color', randomColor());
+		$('#r6').css('color', randomColor());
+		/* Timer for 7 seconds */
+		window.setTimeout(svgColor, 8000);
+	}
+	/* The svgColor function will randomily change the shape color fill */
+	svgColor();  
+});
